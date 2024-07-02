e411e9d4f5b9441e84966a77ddf600d42681aa34
champurad0 <124223013+champurad0@users.noreply.github.com>
2024-07-02 15:58:45 -0700

    added login link and form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -11,3 +12,9 @@ def index():
         {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login/")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
